8beb44d133de065b7a4e084cedc8aa55604c6ff6
GabrieleGrossi <grossigabriele98@gmail.com>
2023-05-15 16:08:34 +0200

    error in console.log

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
-const userKm = parseInt(document.getElementById('user-km'));
-console.log(id='user-km');
+const userKm = parseInt(document.getElementById('userKm'));
+console.log(id='userKm');
 
-const userAge = parseInt(document.getElementById('user-age'));
-console.log(id='user-age');
+const userAge = parseInt(document.getElementById('userAge'));
+console.log(id='userAge');
 
-const button = document.querySelector('button.button')
+const ticketPrice = 0.233;
 
-const ticketPrice = 0.233
+const totalPrice = userKm * ticketPrice
+console.log(totalPrice);
 
-if (userAge < 18){
-    discountPrice = totalPrice - (totalPrice * 0.194);
-    console.log('Essendo ancora minorenne, hai diritto ad uno sconto del 19,4% usl prezzo del biglietto')
-    console.log(discountPrice)
-    document.getElementById("discount").innerHTML = discountPrice.toFixed(2);
-} else if (userAge > 65){
-    discountPrice = totalPrice - (totalPrice * 0.377);
-    console.log('Essendo over 65, hai diritto ad uno sconto del 37,7% sul prezzo del biglietto')
-    console.log(discountPrice)
-    document.getElementById("discount").innerHTML = discountPrice.toFixed(2)
-}
+let discountPrice;
+
+const button = document.querySelector('button.button');
+
+button.addEventListener('click', function() {
+    if (userAge < 18){
+        discountPrice = totalPrice - (totalPrice * 0.194);
+        console.log('Essendo ancora minorenne, hai diritto ad uno sconto del 19,4% usl prezzo del biglietto')
+        console.log(discountPrice)
+        document.getElementById("discount").innerHTML = discountPrice.toFixed(2);
+    } else if (userAge > 65){
+        discountPrice = totalPrice - (totalPrice * 0.377);
+        console.log('Essendo over 65, hai diritto ad uno sconto del 37,7% sul prezzo del biglietto')
+        console.log(discountPrice)
+        document.getElementById("discount").innerHTML = discountPrice.toFixed(2)
+    }
+})
+
