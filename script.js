b884667f203f3dd0899ebb36f911f3c42154b6ee
GabrieleGrossi <grossigabriele98@gmail.com>
2023-05-15 18:09:10 +0200

    add some style and finished the ex

--- script.js
+++ script.js
@@ -14,13 +14,13 @@ button.addEventListener('click',
 
     if (userAge <= 17) {
         console.log(" Ciao, ho visto che hai meno di 18 anni e devi percorrere " + userKm + " km, il tuo biglietto costa " + priceUnder.toFixed(2) + "€. E' stato applicato al prezzo totale uno sconto del 19.4%")
-         answer.innerHTML = " Ciao, ho visto che hai meno di 18 anni e devi percorrere " + userKm + " km, il tuo biglietto costa " + priceUnder.toFixed(2) + "€. E' stato applicato al prezzo totale uno sconto del 19.4%"                
+         answer.innerHTML += " Ciao, ho visto che hai meno di 18 anni e devi percorrere " + userKm + " km, il tuo biglietto costa " + priceUnder.toFixed(2) + "€. E' stato applicato al prezzo totale uno sconto del 19.4%"                
     } else if (userAge >=65) {
         console.log(" Ciao, ho visto che hai più di 65 anni e devi percorrere " + userKm + "km, il tuo biglietto costa " + priceOver.toFixed(2) + "€. E' stato applicato al prezzo totale uno sconto del 37.7%")
-        answer.innerHTML = " Ciao, ho visto che hai più di 65 anni e devi percorrere " + userKm + "km, il tuo biglietto costa " + priceOver.toFixed(2) + "€. E' stato applicato al prezzo totale uno sconto del 37.7%" 
+        answer.innerHTML += " Ciao, ho visto che hai più di 65 anni e devi percorrere " + userKm + "km, il tuo biglietto costa " + priceOver.toFixed(2) + "€. E' stato applicato al prezzo totale uno sconto del 37.7%" 
     } else {
         console.log(" Ciao, ho visto che hai" + userAge + "anni e devi percorrere " + userKm + "km. Il prezzo del biglietto è di " + priceTicket.toFixed(2) + "€. Non è stato possibile applicare sconti per l'età.")
-        answer.innerHTML = " Ciao, ho visto che hai" + userAge + "anni e devi percorrere " + userKm + "km. Il prezzo del biglietto è di " + priceTicket.toFixed(2) + "€. Non è stato possibile applicare sconti per l'età."
+        answer.innerHTML += " Ciao, ho visto che hai " + userAge + " anni e devi percorrere " + userKm + "km. Il prezzo del biglietto è di " + priceTicket.toFixed(2) + "€. Non è stato possibile applicare sconti per l'età."
     }
-    }
+}
 )
